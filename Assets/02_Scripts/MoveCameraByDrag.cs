df468c484b6eaf3ac87702ec58b9de08a8283336
tube0330 <zien0516@naver.com>
2024-09-30 12:47:25 +0900

    0930_마우스를 드래그하여 카메라를 좌우로 이동

---
+++ Assets/02_Scripts/MoveCameraByDrag.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraByDrag : MonoBehaviour
+{
+    Transform tr;
+    [SerializeField] private float speed = 5f;
+    private float firstClickPointX = 0f;
+    private Camera cam;
+
+    void Start()
+    {
+        tr = transform;
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        MoveCamer();
+    }
+
+    void MoveCamer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstClickPointX = Input.mousePosition.x;
+            Debug.Log(firstClickPointX);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 pos = cam.ScreenToViewportPoint(- new Vector2(Input.mousePosition.x - firstClickPointX, 0));
+            Vector2 move = pos * Time.deltaTime * speed;
+
+            cam.transform.Translate(move);
+        }
+    }
+}
