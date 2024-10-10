b6dce44e3e16e0e8f526b827c5a2d3ab19d0c15c
tube0330 <zien0516@naver.com>
2024-10-10 12:50:18 +0900

    .

--- Assets/02.Scripts/Common/GameManage.Field.cs
+++ Assets/02.Scripts/Common/GameManage.Field.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using TMPro;
 
 public partial class GameManage : MonoBehaviour
-{
-
-
-
-
+{    
+    public Canvas canvas_score;
+    [SerializeField] private Sprite star_l;
+    [SerializeField] private Sprite star_m;
+    [SerializeField] private Sprite star_r;
+    private GameObject empty_starL;
+    private GameObject empty_starM;
+    private GameObject empty_starR;
+    private TMP_Text scoreText;
+    private int initScore;
+    public int score;
 }
--- Assets/02.Scripts/Common/GameManage.cs
+++ Assets/02.Scripts/Common/GameManage.cs
@@ -1,22 +1,10 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public partial class GameManage : MonoBehaviour
 {
     public static GameManage Instance;
-
-    private Transform tr;
-    [SerializeField] private Sprite star_l;
-    [SerializeField] private Sprite star_m;
-    [SerializeField] private Sprite star_r;
-    private GameObject empty_starL;
-    private GameObject empty_starM;
-    private GameObject empty_starR;
-    private TMP_Text scoreText;
-
-    private int score = 50000;
-    public int initScore = 0;
 
     void Awake()
     {
@@ -25,12 +13,14 @@ public partial class GameManage : MonoBehaviour
 
     void Start()
     {
-        tr = transform;
+        canvas_score = GameObject.Find("Canvas_score").GetComponent<Canvas>();
         Transform starTr = GameObject.Find("Canvas_score").transform.GetChild(0).GetChild(0);
         empty_starL = starTr.GetChild(0).gameObject;
         empty_starM = starTr.GetChild(1).gameObject;
         empty_starR = starTr.GetChild(2).gameObject;
         scoreText = GameObject.Find("Canvas_score").transform.GetChild(0).GetChild(2).GetComponent<TMP_Text>();
+
+        score = initScore;
     }
 
     void Update()
