af876378250148ce419a60ce50ff2884de496bd3
tube0330 <zien0516@naver.com>
2024-10-10 12:17:47 +0900

    1010_점수 시스템 구현: 점수에 따라 별 개수를 동적으로 표시하는 UI 생성

--- Assets/02.Scripts/Common/GameManage.cs
+++ Assets/02.Scripts/Common/GameManage.cs
@@ -1,12 +1,58 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public partial class GameManage : MonoBehaviour
 {
     public static GameManage Instance;
-    
+
+    private Transform tr;
+    [SerializeField] private Sprite star_l;
+    [SerializeField] private Sprite star_m;
+    [SerializeField] private Sprite star_r;
+    private GameObject empty_starL;
+    private GameObject empty_starM;
+    private GameObject empty_starR;
+    private TMP_Text scoreText;
+
+    private int score = 50000;
+    public int initScore = 0;
+
     void Awake()
     {
         Instance = this;
+    }
+
+    void Start()
+    {
+        tr = transform;
+        Transform starTr = GameObject.Find("Canvas_score").transform.GetChild(0).GetChild(0);
+        empty_starL = starTr.GetChild(0).gameObject;
+        empty_starM = starTr.GetChild(1).gameObject;
+        empty_starR = starTr.GetChild(2).gameObject;
+        scoreText = GameObject.Find("Canvas_score").transform.GetChild(0).GetChild(2).GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        if (score >= 10000)
+        {
+            scoreText.text = $"{score}";
+            empty_starL.GetComponent<Image>().sprite = star_l;
+        }
+
+        if (score >= 23000)
+        {
+            scoreText.text = $"{score}";
+            empty_starL.GetComponent<Image>().sprite = star_l;
+            empty_starM.GetComponent<Image>().sprite = star_m;
+        }
+        if (score >= 40000)
+        {
+            scoreText.text = $"{score}";
+            empty_starL.GetComponent<Image>().sprite = star_l;
+            empty_starM.GetComponent<Image>().sprite = star_m;
+            empty_starR.GetComponent<Image>().sprite = star_r;
+        }
     }
 }
--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,19 @@ particle system simulation speed를 올리면 좀 낫긴한데 흠,.,.
 shape angle 올릴수록 길어짐
 
 
-게임 끝나는 조건이 pig가 다 없어졌는지
+게임 끝나는 조건이 pig(5000)가 다 없어졌는지
 그러면 이제 UI 나타나서 prebird가 남은 수만큼 + 10000 해서 score에 넣으면 됨
+bubble 300
+얼음소행성 big 파괴 2300
+얼음소행성 medium 파괴 1300
+얼음소행성 small 파괴 800
 
 
+
+
+1단계
+prebird 3개
+
+1만 1개
+2만3천 2개
+4만부터 별3개
